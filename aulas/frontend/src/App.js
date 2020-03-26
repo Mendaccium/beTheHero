9e60ec455797904364677936e77bcfa2cc0b0e45
Mendaccium <antunes.f@acad.ifma.edu.br>
2020-03-26 16:53:31 -0300

    backend

---
+++ aulas/frontend/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          SERIO Q COMPILA EM TEMPO REAL?
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Segue LAAAA
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
